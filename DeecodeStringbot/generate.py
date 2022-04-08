857ce28b2f7ce87ded30c37267d5977f46d8c195
Rakesh Meena <82395901+rakeshyt@users.noreply.github.com>
2022-04-08 20:10:09 +0530

    Update generate.py

--- DeecodeStringbot/generate.py
+++ DeecodeStringbot/generate.py
@@ -115,7 +115,7 @@ async def generate_session(bot, msg, telethon=False):
     except KeyError:
         pass
     await client.disconnect()
-    await phone_code_msg.reply("Successfully generated {} string session. \n\nPlease check your saved messages! \n\nBy @Thenavya".format("telethon" if telethon else "pyrogram"))
+    await phone_code_msg.reply("Successfully generated {} string session. \n\n`{}`\n\nPlease check your saved messages! \n\nBy @Thenavya".format("telethon" if telethon else "pyrogram", string_session))
 
 
 async def cancelled(msg):
