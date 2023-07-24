a934b7d6c62f3abc94084cb72af9e7a61b7d8f87
Si Arab - <98678420+fadhilabdat04@users.noreply.github.com>
2023-07-25 01:29:13 +0700

    Update Config.py

--- Config.py
+++ Config.py
@@ -20,7 +20,7 @@ else:
     API_ID = 12857763
     API_HASH = "7b71e8bca0d5e1c6d8383ae818d9ec8d"
     BOT_TOKEN = "6375262996:AAFZBXDHSjs6Yn5M_1CbV_XS-tWQqG_wYX4"
-    DATABASE_URL = "mongodb+srv://Arab01:fadhil123@cluster0.ul5qaif.mongodb.net/"
+    DATABASE_URL = "postgres://kprfogjs:z1cQQkrgTItmMGBVBbNQIJ3HWhY8kFRT@arjuna.db.elephantsql.com/kprfogjs"
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
     MUST_JOIN = "SiArab_Store"
     if MUST_JOIN.startswith("@"):
